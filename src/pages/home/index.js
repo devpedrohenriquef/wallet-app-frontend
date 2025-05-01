9552c89b7bf04f1047be25abc3a42b69874b8e8e
Pedro Henrique Marques do Nascimento Ferreira <pedro@MacBook-Air-de-Pedro.local>
2025-05-01 15:46:35 -0300

    feat: add delete finance functionality and UI integration

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,3 +1,23 @@
+const onDeleteItem = async (id) => {
+  console.log(id);
+  try {
+    const email = localStorage.getItem("@WalletApp:userEmail");
+    await fetch(
+      `https://wallet-app-api-mp-16500a81b5f4.herokuapp.com/finances/${id}`,
+      {
+        method: "DELETE",
+        headers: {
+          email: email,
+        },
+      }
+    );
+    onLoadFinancesData();
+  } catch (error) {
+    console.error(error);
+    alert("Erro ao deletar item.");
+  }
+};
+
 const renderFinancesList = (data) => {
   const table = document.getElementById("finances-table");
   table.innerHTML = "";
@@ -71,6 +91,8 @@ const renderFinancesList = (data) => {
 
     // Delete
     const deleteTd = document.createElement("td");
+    deleteTd.style.cursor = "pointer";
+    deleteTd.onclick = () => onDeleteItem(item.id);
     deleteTd.className = "right";
     const deleteText = document.createTextNode("Deletar");
     deleteTd.appendChild(deleteText);
